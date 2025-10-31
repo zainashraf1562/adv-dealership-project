de8159c7b7726fa7f5bbef3ccf70bb6c97f408e7
Zain Ashraf <zainashraf1562@gmail.com>
2025-10-31 11:53:03 -0400

    finished contract

--- src/main/java/com/pluralsight/Contract.java
+++ src/main/java/com/pluralsight/Contract.java
@@ -1,5 +1,52 @@
 package com.pluralsight;
 
 public abstract class Contract {
+    private String date;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicleSold;
+
+    public Contract(String date, String customerName, String customerDate, Vehicle vehicleSold) {
+        this.date = date;
+        this.customerName = customerName;
+        this.customerEmail = customerDate;
+        this.vehicleSold = vehicleSold;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public Vehicle getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(Vehicle vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+
+    public abstract double getMonthlyPayment();
+
+    public abstract double getTotalPrice();
 
 }
